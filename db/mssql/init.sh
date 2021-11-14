741657e8f4c33a5407eee94f62dcdc1d9185ded7
shahriar0104 <32048867+shahriar0104@users.noreply.github.com>
2021-11-15 00:33:23 +0600

    update ci.yml & path changed

--- db/mssql/init.sh
+++ db/mssql/init.sh
@@ -3,4 +3,4 @@ sleep 30s
 
 echo "running set up script"
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P toor0104~ -d master -i db-init.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P toor0104~ -d master -i todo.sql
